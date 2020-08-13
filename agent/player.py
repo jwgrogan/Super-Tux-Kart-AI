3a695f900ed8fefbf64d3b6bee07624627e97248
Jake Grogan <jagrogan@microsoft.com>
2020-08-12 20:52:59 -0700

    controller updates

--- agent/player.py
+++ agent/player.py
@@ -3,16 +3,18 @@ from .model import PuckDetector, load_model
 import torchvision.transforms.functional as F
 import torch
 
-
+# helper functions
 def to_numpy(location):
     """
     Don't care about location[1], which is the height
     """
     return np.float32([location[0], location[2]])
+
 def _to_image(x, proj, view):
     W, H = 400, 300
     p = proj @ view @ np.array(list(x) + [1])
     return np.array([W / 2 * (p[0] / p[-1] + 1), H / 2 * (1 - p[1] / p[-1])])
+
 def get_vector_from_this_to_that(me, obj, normalize=True):
     """
     Expects numpy arrays as input
@@ -21,6 +23,28 @@ def get_vector_from_this_to_that(me, obj, normalize=True):
     if normalize:
         return vector / np.linalg.norm(vector)
     return vector
+
+def _to_world(x, y, proj, view, height=0):
+        W, H = 400, 300
+        pv_inv = np.linalg.pinv(proj @ view)
+        xy, d = pv_inv.dot([float(x) / (W / 2) - 1, 1 -
+                            float(y) / (H / 2), 0, 1]), pv_inv[:, 2]
+        x0, x1 = xy[:-1] / xy[-1], (xy+d)[:-1] / (xy+d)[-1]
+        t = (height-x0[1]) / (x1[1] - x0[1])
+        if t < 1e-3 or t > 10:
+            # Project the point forward by a certain distance, if it would end up behind
+            t = 10
+        return t * x1 + (1-t) * x0
+
+def image_to_local(x, y, player):
+        y += 40
+        proj = np.array(player.camera.projection).T
+        view = np.array(player.camera.view).T
+        x, _, y = self._to_world(x, y, proj, view, player.kart.location[1])
+        # strip ridiculous values
+        if abs(x) > 39 or abs(y) > 64:
+            return (99, 99)
+        return (x, y)
 
 class HockeyPlayer:
     """
@@ -48,7 +72,7 @@ class HockeyPlayer:
         # set up parameters
         self.state = 'kickoff'
         # self.states = {'kickoff': self.kickoff_action,
-        #                 'reset': self.reset_action,
+        #                 'positioning': self.positioning_action,
         #                 'attack': self.attack_action,
         #                 'stuck': self.stuck_action
         #                 }
@@ -92,6 +116,29 @@ class HockeyPlayer:
         if normalize:
             return vector / np.linalg.norm(vector)
         return vector
+    
+    
+    def _to_world(self, x, y, proj, view, height=0):
+        W, H = 400, 300
+        pv_inv = np.linalg.pinv(proj @ view)
+        xy, d = pv_inv.dot([float(x) / (W / 2) - 1, 1 -
+                            float(y) / (H / 2), 0, 1]), pv_inv[:, 2]
+        x0, x1 = xy[:-1] / xy[-1], (xy+d)[:-1] / (xy+d)[-1]
+        t = (height-x0[1]) / (x1[1] - x0[1])
+        if t < 1e-3 or t > 10:
+            # Project the point forward by a certain distance, if it would end up behind
+            t = 10
+        return t * x1 + (1-t) * x0
+
+    def image_to_local(self, x, y, player):
+        y += 40
+        proj = np.array(player.camera.projection).T
+        view = np.array(player.camera.view).T
+        x, _, y = self._to_world(x, y, proj, view, player.kart.location[1])
+        # strip ridiculous values
+        if abs(x) > 39 or abs(y) > 64:
+            return (99, 99)
+        return (x, y)
 
     def set_goal_loc(self, kart_loc):
         z = kart_loc[-1]
@@ -106,12 +153,19 @@ class HockeyPlayer:
         """
         set current state of the kart
         """
-        if self.past_locs == None or self.kickoff(kart_loc) == True:
+        if len(self.past_locs) == 0 or self.kickoff(kart_loc) == True:
             self.state = 'kickoff'
-        elif self.stuck(kart_loc):
+            self.kickoff_timer = 0
+        else:
+            self.kickoff_timer += 1
+
+        if self.state == 'kickoff' and self.kickoff_timer < 60:
+            self.state = 'kickoff'
+
+        if self.stuck(kart_loc) == True:
             self.state == 'stuck'
-        elif self.reset(kart_loc) == True:
-            self.state = 'reset'
+        elif self.positioning(kart_loc) == True:
+            self.state = 'positioning'
         elif self.attack_position(kart_loc, puck_loc) == True:
             self.state = 'attack'
         return self.state
@@ -132,27 +186,36 @@ class HockeyPlayer:
         return action
         
     def kickoff(self, kart_loc):
-        threshold = 20
-        for past_locs in reversed(self.past_locs):
-            x_diff = abs(past_locs[0] - kart_loc[0])
-            z_diff = abs(past_locs[-1] - kart_loc[-1])
-            if x_diff > threshold or z_diff > threshold:
-                return True
+        threshold = 5
+        last_loc = self.past_locs[-1]
+        x_diff = abs(last_loc[0] - kart_loc[0])
+        z_diff = abs(last_loc[-1] - kart_loc[-1])
+        if x_diff > threshold or z_diff > threshold:
+            return True
         return False
 
-    def kickoff_action(self, action):
+    def kickoff_action(self, kart_loc, kart_front, puck_loc, action):
+        """
+        boost towards center to find puck
+        """
+        vector_of_kart = get_vector_from_this_to_that(kart_loc, kart_front)
+        vector_to_goal = get_vector_from_this_to_that(kart_loc, self.their_goal)
+        vector_to_puck = get_vector_from_this_to_that(kart_loc, puck_loc)
+
+        action['steer'] = np.sign(np.dot(vector_of_kart, vector_to_puck))
         action['acceleration'] = 1
         action['nitro'] = True
+        
         return action
 
-    def reset(self, kart_loc):
+    def positioning(self, kart_loc):
         x = kart_loc[0]
         y = kart_loc[-1]
         if x == 0 or x == 2 or y == 0 or y == 2:
             return True
         return False
 
-    def reset_action(self, action):
+    def positioning_action(self, action):
         action['brake'] = True
         action['acceleration'] = 0
         return action   
@@ -167,17 +230,17 @@ class HockeyPlayer:
             return False
 
     def attack_action(self, kart_loc, kart_front, puck_loc, x, action):
-        action['steer'] = 0 #aim towards center
+        
         kart_to_puck = np.linalg.norm(kart_loc - puck_loc)
 
         vector_of_kart = get_vector_from_this_to_that(kart_loc, kart_front)
         vector_to_goal = get_vector_from_this_to_that(kart_loc, self.their_goal)
         vector_to_puck = get_vector_from_this_to_that(kart_loc, puck_loc)
 
-
+        action['steer'] = np.sign(0) * 1#aim towards center
         # x = puck_loc[0]
         if x > .3 or x < -.3:
-                action['steer'] = np.sign(x) * 1
+            action['steer'] = np.sign(x) * 1
         else:
             action['steer'] = 0
         if x > .4 or x < -.4:
@@ -222,14 +285,20 @@ class HockeyPlayer:
         action = {'acceleration': 1, 'brake': False, 'drift': False, 'nitro': False, 'rescue': False, 'steer': 0}
         # Puck Information
         image_transform = F.to_tensor(image)[None]
-        puck_loc = (self.model(image_transform).detach().cpu().numpy())[0]
+        image_puck_loc = (self.model(image_transform).detach().cpu().numpy())[0]
+        
         # print("Puck Location", puck_location)
-        x = puck_loc[0]
-        y = puck_loc[1]
+        x = image_puck_loc[0]
+        y = image_puck_loc[1]
+        
+        puck_loc = np.float32(self.image_to_local(x, y, player_info))
+        print(puck_loc)
 
         x_middle = 0
         # Kart Information
         kart_loc = to_numpy(player_info.kart.location)
+        print(kart_loc.shape())     
+
         kart_front = to_numpy(player_info.kart.front)
         kart_velocity = player_info.kart.velocity
         kart_attachment = player_info.kart.attachment.type
@@ -243,22 +312,25 @@ class HockeyPlayer:
         # set kart state
         self.state = self.set_state(kart_loc, puck_loc)
         print(self.state)
+
         if self.state == 'kickoff':
-            action = self.kickoff_action(action)
+            puck_loc = (0, 0, 0)
+            action = self.kickoff_action(kart_loc, kart_front, puck_loc, action)
         elif self.state == 'attack':
             action = self.attack_action(kart_loc, kart_front, puck_loc, x, action)
-        elif self.state == 'reset':
-            action = self.reset_action(action)
+        elif self.state == 'positioning':
+            action = self.positioning_action(action)
         elif self.state == 'stuck':
             action = self.stuck_action(x, action)
         
         self.past_locs.append(kart_loc)
         self.past_actions.append(action)
+
         # self.position = (player_id - 1 / 2 ) % 2
         # self.teammate_has_puck = False
         # self.step = 0
 
-        return action, puck_loc
+        return action, image_puck_loc
         
         
         
